a3810e85016a4cc9e1afe31ae28a97f6e18d83c7
Ahmed Yahia <85857492+Ahmed-Yahia-cs@users.noreply.github.com>
2022-08-18 15:18:13 +0200

    Time: 48 ms (61.02%), Space: 13.9 MB (63.86%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -1,15 +1,22 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        max_valid = 2**31
+        reversed_num = 0
+        
         # Handeling sign
         sign = 1 if x >= 0 else -1
         x = x if x >= 0 else x * -1
         
         # Handeling the number 
-        number_str = str(x)[::-1]
+        while x > 0:
+            remander = x % 10
+            x = x // 10
+            
+            if max_valid - (reversed_num * 10) < remander:
+                return 0
+            
+            reversed_num = (reversed_num * 10) + remander
+            
         
-        ans = sign * int(number_str)
-        
-        # output checking
-        if ans < -2**31 or ans > 2**31:
-            ans = 0
+        ans = sign * reversed_num
         return ans
